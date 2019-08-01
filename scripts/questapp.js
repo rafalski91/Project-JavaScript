e9dec25b8c41dcadfdc30570d473d094ab98f4ed
Rafal <rafalweglinski@protonmail.com>
2019-08-01 18:41:39 +0200

    api
    add api

--- scripts/questapp.js
+++ scripts/questapp.js
@@ -1,4 +1,4 @@
-let $questList, $questTitle, $questAdd, $pop, $popExit, $popOverlay;
+let $welcomePage, $buttonWelcome, $contApp, $questList, $questTitle, $questAdd, $pop, $popExit, $popOverlay;
 
 let id = 1;
 
@@ -20,6 +20,9 @@ function mainQuest() {
 }
 
 function pickDOMEelements() {
+   $welcomePage = document.getElementById('welcome');
+   $buttonWelcome = document.getElementById('welcomeButton');
+   $contApp = document.getElementById('container');
    document.getElementById("date").innerHTML = d + " / " + m + " / " + y;
    $questList = document.getElementById('list');
    $questTitle = document.getElementById('headtitle');
@@ -30,12 +33,69 @@ function pickDOMEelements() {
 }
 
 function listenerDOMEvents() {
+   $buttonWelcome.addEventListener('click', slideUp);
    $questAdd.addEventListener('click', addClickHandler);
    $questAdd.addEventListener('keyup', enterClickHandler);
    $questList.addEventListener('click', listClickManager);
    $pop.addEventListener('click', popupClickManager);
    $popExit.addEventListener('click', popupClickManager);
 }
+
+function slideUp(eventObject) {
+   if(eventObject.target = $buttonWelcome) {
+     $welcomePage.style.height = '0';
+     $contApp.style.display = "block";
+     $contApp.style.marginTop = "0";
+     getAllQuests();
+   };
+ }
+
+//  async function getQuestsBase() {
+//    let quests = await axios.get('http://195.181.210.249:3000/todo/');
+//    let newElement;
+//    newELement.innerText = quests.data.map(quests => quests.title).join(', ');
+//    $questList.appendChild(newElement);
+// }
+
+// async function getQuestsBase() {
+//    let quests = await axios.get('http://195.181.210.249:3000/todo/');
+//    console.log($questList.firstChild);
+//    let updateElement = createElement();
+//    console.log(updateElement)
+//    updateElement.classList('text').innerText = quests.data.map(quests => quests.title);
+//    console.log(updateElement)
+//    console.log($questList)
+//    $questList.appendChild(updateElement);
+// }
+
+
+async function getAllQuests() {
+   let quests = await axios.get('http://195.181.210.249:3000/todo/?author=RW');
+   var resultElement = $questList;
+   resultElement.innerHTML = '';
+   var uploadElement = createElement();
+   var cos = uploadElement.querySelector('.text');
+   cos.innerText = quests.data.map(quests => quests.title);
+   $questList.appendChild(uploadElement);
+   
+   console.log(quests);
+ }
+
+ async function addNewQuest() {
+   await axios.post('http://195.181.210.249:3000/todo/', {
+      title: $questTitle.value,
+      author: 'RW',
+   });
+ }
+
+ async function delNewQuest() {
+   let quests = await axios.get('http://195.181.210.249:3000/todo/');
+   let lastQuestId = quests.data[quests.data.length -1].id;
+   await axios.delete('http://195.181.210.249:3000/todo/' +lastQuestId, {
+      title: $questTitle.value,
+      author: 'RW',
+   });
+ }
 
 function initialList() {
    intList.forEach(quest => {
@@ -52,7 +112,22 @@ function addButton() {
    };
    if (isEmpty === false) {
       newQuest($questTitle.value);
-      $questTitle.value = "";
+   };
+   addNewQuest();
+}
+
+function enterClickHandler(event) {
+   if (event.keyCode == 13) {
+      var isEmpty = false,
+      tname = $questTitle.value;
+      if (tname === "") {
+         alert("Title cannot by empty");
+         isEmpty = true;
+      };
+      if (isEmpty === false) {
+         newQuest($questTitle.value);
+      };
+      addNewQuest();
    };
 }
 
@@ -95,6 +170,7 @@ function removeItem(eventObject) {
       var parent = eventObject.target.parentElement.parentElement;
       parent.remove(this);
    };
+   delNewQuest();
 }
 
 function doneItem(eventObject) {
@@ -143,16 +219,6 @@ function popupCancel(eventObject) {
 function addClickHandler(eventObject) {
    if (eventObject.target.className === 'headadd') {
       addButton(eventObject);
-   };
-}
-
-function enterClickHandler(event) {
-   if (event.keyCode == 13) {
-      const title = $questTitle.value;
-      if (title) {
-         newQuest(title);
-      };
-      $questTitle.value = "";
    };
 }
 
