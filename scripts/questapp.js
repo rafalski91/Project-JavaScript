6f2f9b9e8e4e161cc9cf813d77a65b241cc7cc86
Rafal <rafalweglinski@protonmail.com>
2019-08-28 13:59:24 +0200

    update questapp

--- scripts/questapp.js
+++ scripts/questapp.js
@@ -1,15 +1,17 @@
+//-------- let $sample = DOM ELEMENTS ----------------------------->
+
 let $welcomePage, $buttonWelcome, $contApp, $questList, $questTitle, $questAdd, $pop, $popExit, $popOverlay;
 
-let date;
-n = new Date();
-y = n.getFullYear();
-const monthNames = ["January", "February", "March", "April", "May", "June",
-   "July", "August", "September", "October", "November", "December"
-];
-m = monthNames[n.getMonth() + 1];
-d = n.getDate();
+//-------- UPLOAD LOCAL DATE ----------------------------->
 
-const intList = ['Dodawaj, edytuj, usuwaj, oznaczaj zadania'];
+let currentDate = new Date();
+y = currentDate.getFullYear();
+const monthNames = ["January", "February", "March", "April", "May", "June",
+   "July", "August", "September", "October", "November", "December"];
+m = monthNames[currentDate.getMonth() + 1];
+d = currentDate.getDate();
+
+//---------------------------------------------------------->
 
 function mainQuest() {
    pickDOMEelements();
@@ -32,12 +34,14 @@ function pickDOMEelements() {
 
 function listenerDOMEvents() {
    $buttonWelcome.addEventListener('click', slideUp);
-   $questAdd.addEventListener('click', addClickHandler);
-   $questAdd.addEventListener('keyup', enterClickHandler);
+   $questAdd.addEventListener('click', addButtonListener);
+   $questAdd.addEventListener('keyup', addButtonListener);
    $questList.addEventListener('click', listClickManager);
    $pop.addEventListener('click', popupClickManager);
    $popExit.addEventListener('click', popupClickManager);
 }
+
+//-------- OPEN SLIDE ----------------------------->
 
 function slideUp(eventObject) {
    if(eventObject.target = $buttonWelcome) {
@@ -48,27 +52,27 @@ function slideUp(eventObject) {
    };
  }
 
+ //-------- API SERVER ----------------------------->
+
 async function getAllQuests() {
    let quests = await axios.get('http://195.181.210.249:3000/todo/');
-   // var resultElement = $questList;
    // // resultElement.innerHTML = '';
    // filter(quest => quest.author === 'RW').
     quests.data.filter(quest => quest.author === 'RW').forEach(quest => {
       newQuest(quest.title, quest.id, quest.extra)});
-   // $questList.appendChild(resultElement);
    console.log(quests.data.filter(quest => quest.author === 'RW'))
    
  }
 
  async function addNewQuest(id) {
-   await axios.post('http://195.181.210.249:3000/todo/' + id, {
+   await axios.post('http://195.181.210.249:3000/todo/' + id), {
       title: $questTitle.value,
       author: 'RW',
-   });
+   };
  }
 
- async function delQuest(id) {
-   await axios.delete('http://195.181.210.249:3000/todo/' + id);
+ async function delQuest() {
+   await axios.delete('http://195.181.210.249:3000/todo/');
    $questList.innerHTML = '';
    getAllQuests();
  }
@@ -79,30 +83,19 @@ async function getAllQuests() {
    }
  }
 
+//-------- ADD / CREATE NEW TASK ----------------------------->
+
 function initialList() {
+   const intList = ['Dodawaj, edytuj, usuwaj, oznaczaj zadania'];
    intList.forEach(quest => {
       newQuest(quest);
    });
 }
 
-function addButton() {
-   var isEmpty = false,
-      tname = $questTitle.value;
-   if (tname === "") {
-      alert("Title cannot by empty");
-      isEmpty = true;
-   };
-   if (isEmpty === false) {
-      newQuest($questTitle.value);
-      
-   };
-   addNewQuest();
-}
-
-function enterClickHandler(event) {
-   if (event.keyCode == 13) {
-      var isEmpty = false,
-      tname = $questTitle.value;
+function addButtonListener(event) {
+   if (event.keyCode == 13 || event.target.className === 'headadd') {
+      let isEmpty = false;
+      let tname = $questTitle.value;
       if (tname === "") {
          alert("Title cannot by empty");
          isEmpty = true;
@@ -147,26 +140,16 @@ function createElement(title, id) {
    return newElement;
 }
 
-function removeItem(eventObject, idToDelete) {
+//-------- BUTTONS ----------------------------->
+
+function removeItem(eventObject, id) {
    let delBtn = eventObject.target.classList.contains('delete');
    if (delBtn === true) {
       var parent = eventObject.target.parentElement.parentElement;
       console.log(parent);
-      parent.remove(idToDelete);
+      parent.remove(id);
    };
-   delQuest(idToDelete);
-}
-
-function doneItem(eventObject) {
-   let doneBtn = eventObject.target.classList.contains('done');
-   var parents = eventObject.target.parentElement.parentElement;
-   if (doneBtn === true && parents.firstChild.style.textDecoration !== 'line-through') {
-      parents.firstChild.style.textDecoration = 'line-through',
-      pareDone = true;
-   } else if (parents.firstChild.style.textDecoration == 'line-through') {
-      parents.firstChild.style.textDecoration = 'none',
-      pareDone = false;
-   };
+   delQuest();
 }
 
 function editItem(eventObject) {
@@ -178,42 +161,15 @@ function editItem(eventObject) {
    };
 }
 
-function addText(eventObject) {
-   let popupAdd = eventObject.target.classList.contains('popupadd');
-   var editText = eventObject.target.parentNode.querySelector('#popuptitle').value;
-   var editedText = document.querySelector('.newText');
-   if (popupAdd === true) {
-      $pop.style.display = 'none';
-      $popExit.style.display = 'none';
-      $popOverlay.style.display = 'none';
-      editedText.innerText = editText;
-   };
-   var extraClass = document.querySelector('.newText');
-   extraClass.classList.remove('newText');
-}
-
-function popupCancel(eventObject) {
-   let popupCancel = eventObject.target.classList.contains('popupcancel');
-   let popupExit = eventObject.target.classList.contains('fas');
-   if (popupCancel === true || popupExit === true) {
-      $pop.style.display = 'none';
-      $popExit.style.display = 'none';
-      $popOverlay.style.display = 'none';
-   };
-}
-
-function addClickHandler(eventObject) {
-   if (eventObject.target.className === 'headadd') {
-      addButton(eventObject);
-   };
-}
-
-function popupClickManager(eventObject) {
-   if (eventObject.target.className === 'popupcancel' || eventObject.target.className === 'fas fa-times') {
-      popupCancel(eventObject);
-   };
-   if (eventObject.target.className === 'popupadd') {
-      addText(eventObject);
+function doneItem(eventObject) {
+   let doneBtn = eventObject.target.classList.contains('done');
+   var parents = eventObject.target.parentElement.parentElement;
+   if (doneBtn === true && parents.firstChild.style.textDecoration !== 'line-through') {
+      parents.firstChild.style.textDecoration = 'line-through',
+      pareDone = true;
+   } else if (parents.firstChild.style.textDecoration == 'line-through') {
+      parents.firstChild.style.textDecoration = 'none',
+      pareDone = false;
    };
 }
 
@@ -232,6 +188,41 @@ function listClickManager(eventObject) {
    if (eventObject.target.className === 'done') {
       doneItem(eventObject);
    };
+}
+
+//-------- POPUP ----------------------------->
+
+function popupClickManager(eventObject) {
+   if (eventObject.target.className === 'popupcancel' || eventObject.target.className === 'fas fa-times') {
+      popupCancel(eventObject);
+   };
+   if (eventObject.target.className === 'popupadd') {
+      addText(eventObject);
+   };
+}
+
+function popupCancel(eventObject) {
+   let popupCancel = eventObject.target.classList.contains('popupcancel');
+   let popupExit = eventObject.target.classList.contains('fas');
+   if (popupCancel === true || popupExit === true) {
+      $pop.style.display = 'none';
+      $popExit.style.display = 'none';
+      $popOverlay.style.display = 'none';
+   };
+}
+
+function addText(eventObject) {
+   let popupAdd = eventObject.target.classList.contains('popupadd');
+   var editText = eventObject.target.parentNode.querySelector('#popuptitle').value;
+   var editedText = document.querySelector('.newText');
+   if (popupAdd === true) {
+      $pop.style.display = 'none';
+      $popExit.style.display = 'none';
+      $popOverlay.style.display = 'none';
+      editedText.innerText = editText;
+   };
+   var extraClass = document.querySelector('.newText');
+   extraClass.classList.remove('newText');
 }
 
 document.addEventListener('DOMContentLoaded', mainQuest);
